cceb12d593b53c3905e4ef83a0b2bf1de124e7ee
lexbridge21 <stenshornbernadette09@gmail.com>
2025-08-12 16:15:44 +0100

    first commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.svg"; // or .png if you're still using PNG
 
 export default function Logo() {
   return (
     <Link to="/" className="flex items-center">
       <img
-        src={logo}
-        alt="BB Flying Adventure LLC"
+        src="/logo.svg"
+        alt="BB Travel Logo"
         className="h-auto w-32 sm:w-36 md:w-38 lg:w-45 xl:w-48  object-contain font-chiswick font-normal"
       />
     </Link>
